6fe34dfa907388b8e3590240ddd05bd625f4e0fd
Alaa.N.Duridi <51034664+AlaaDuridi@users.noreply.github.com>
2023-03-12 18:59:08 +0200

    Update atm.js

--- atm.js
+++ atm.js
@@ -1,82 +1,67 @@
-// Constants
-const PIN_LENGTH = 4;
-const CORRECT_PIN = "1234";
-const CORRECT_PASS ="123456789" ;
-var  MAX_WITHDRAWAL = 500;
+var arr = [
+    { "pin":"1234", "pass":"1020", "balance":"1000",},
+    { "pin":"5678", "pass":"1020", "balance":"500", },
+    { "pin":"9123", "pass":"1020", "balance":"700", },
+];
+let enteredPin = "";
+let enteredPassword = "";
+let  position =-1; 
+var enteredAmount;
+var x = 1;
+/* variable x is because the submit button , can be used for two operations */ 
+function enterPin() {
+    enteredPin = document.getElementById('pin').value;
+    enteredPassword = document.getElementById('pass').value;
+    var flag =false ;
+    if (!enteredPin || !enteredPassword) {
+        alert('password and pin are required '); return;
+    }
 
-let enteredPin ="" ;
-let enteredPassword="" ; 
-var  enteredAmount  ; 
-
-var x =1 ;
-
-function enterPin() { 
-enteredPin = document.getElementById('pin').value ; 
-enteredPassword = document.getElementById('pass').value ; 
-if(!enteredPin || !enteredPassword) { 
-
-    alert('password and pin are required ') ;return  ; 
-
+    for(var i =0 ;i<arr.length ;i++) { 
+        if(enteredPin == arr[i].pin){
+            position =i ;flag =true ;
+        }
+    }
+    if(!flag) {
+        alert('Invalid card number!') ; return ;
+    }
+    if( enteredPassword != arr[position].pass){
+        alert('Wrong Password!') ;return ;
+    }
+        document.getElementById('entry').style.display = "none";
+        document.getElementById('favLanguageRadioButton').style.display = "block";
 }
-
-if(enteredPin != CORRECT_PIN || enteredPassword != CORRECT_PASS ) {
-
-    alert('pin or password are notcorrect '); 
-}
-else  { 
-
-    document.getElementById('entry').style.display ="none" ; 
-    document.getElementById('favLanguageRadioButton').style.display="block" ; 
-    document.getElementById('options').style.display="block" ; 
-    // document.getElementById('neededMoney').style.display="none" ; 
-    // document.getElementById('submit').style.display="none" ; 
-
-}
-}
-
 function showw() {
-    document.getElementById('neededMoney').style.display="block" ; 
-    document.getElementById('submit').style.display="block" ;
-    document.getElementById('neededMoney').value ="" ; 
-
+    document.getElementById('neededMoney').value = "";
+    document.getElementById('neededMoney').style.display = "block";
+    document.getElementById('submit').style.display = "block";
 }
-function withdraw() { 
-    x=1 ; 
-    showw() ;
+function withdraw() {
+    x = 1;
+    showw();
 }
-function withdrawgo() { 
-    enteredAmount =  document.getElementById('neededMoney').value;
-
-    if(x==1) {
-    if(!enteredAmount) {
-
-    alert('no money needed') ;return ; 
+function dosth() {
+    enteredAmount =Number(document.getElementById('neededMoney').value );
+    if(!enteredAmount || enteredAmount<=0 ){
+        alert('Invalid Amount Of Money!') ; return ; 
     }
-    if(enteredAmount > MAX_WITHDRAWAL)
-    {
-        alert('you entered invalid amount of money ! this is too much -_-') ; return ; 
-    
+    if (x == 1) {
+        if (enteredAmount > arr[position].balance) {
+            alert('Invalid Amount Of Money!') ; return ; 
+        }
+        arr[position].balance = Number(arr[position].balance) - Number(enteredAmount);
     }
-        MAX_WITHDRAWAL = Number(MAX_WITHDRAWAL) - Number(enteredAmount) ;  
+    else {
+        arr[position].balance = Number(arr[position].balance) + Number(enteredAmount);
+    }
+    alert('Transaction Succeeded, your current balance is ' + arr[position].balance);
 }
-else { 
-    if(!enteredAmount) {
-        alert('no money entered ') ;return ;} 
-        
-        MAX_WITHDRAWAL = Number(enteredAmount) + Number(MAX_WITHDRAWAL) ; 
-
-}
-alert('the process succeeded , your current balance is ' + MAX_WITHDRAWAL) ; 
-
-}
-
-function diposit() { 
+function diposit() {
+    showw();
     x=0 ;
-    showw() ;
 }
-function balance() { 
-    alert('Your current balance is ' + MAX_WITHDRAWAL) ;
-    document.getElementById('neededMoney').style.display="none" ; 
-    document.getElementById('submit').style.display="none" ; 
+function balance() {
+    alert('Your current balance is ' +arr[position].balance);
+    document.getElementById('neededMoney').style.display = "none";
+    document.getElementById('submit').style.display = "none";
 }
-    
